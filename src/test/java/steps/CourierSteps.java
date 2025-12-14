04b98ca239d999ae3e17c7a7195b707ef4eebb81
Max <angrymaxim777@gmail.com>
2025-12-14 22:03:18 +0400

    Add CourierSteps

---
+++ src/test/java/steps/CourierSteps.java
@@ -0,0 +1,64 @@
+package steps;
+
+import api.CourierApi;
+import dto.CourierDto;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class CourierSteps {
+    private final CourierApi courierApi = new CourierApi();
+
+    @Step("Создать курьера с логином {courier.login}, паролем {courier.password} и именем {courier.firstName}")
+    public Response createCourier(CourierDto courier) {
+        return courierApi.createCourier(courier);
+    }
+
+    @Step("Проверить успешное создание курьера (код 201, ok: true)")
+    public void checkCourierCreatedSuccessfully(Response response) {
+        response.then()
+                .statusCode(201)
+                .body("ok", equalTo(true));
+    }
+
+    @Step("Проверить ошибку при создании курьера (код 400): Недостаточно данных для создания учетной записи")
+    public void checkCourierCreationError(Response response) {
+        response.then()
+                .statusCode(400)
+                .body("message", equalTo("Недостаточно данных для создания учетной записи"));
+    }
+
+    @Step("Проверить конфликт при создании дубликата курьера (код 409): Этот логин уже используется")
+    public void checkDuplicateCourierError(Response response) {
+        response.then()
+                .statusCode(409)
+                .body("message", equalTo("Этот логин уже используется"));
+    }
+
+    @Step("Авторизовать курьера с логином {login}")
+    public Response loginCourier(String login, String password) {
+        return courierApi.loginCourier(login, password);
+    }
+
+    @Step("Проверить успешную авторизацию (код 200, есть id)")
+    public String checkLoginSuccessAndGetId(Response response) {
+        response.then()
+                .statusCode(200)
+                .body("id", notNullValue());
+        return response.jsonPath().getString("id");
+    }
+
+    @Step("Проверить ошибку авторизации (код 400): Недостаточно данных для входа")
+    public void checkLoginError(Response response) {
+        response.then()
+                .statusCode(400)
+                .body("message", equalTo("Недостаточно данных для входа"));
+    }
+
+    @Step("Удалить курьера с id {id}")
+    public Response deleteCourier(String id) {
+        return courierApi.deleteCourier(id);
+    }
+}
